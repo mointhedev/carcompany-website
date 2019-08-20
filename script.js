3e9c50028e07ccf7a7493aab072e37e018a2946a
mointhedev <52953600+moin248@users.noreply.github.com>
2019-08-20 14:57:30 +0500

    Fixed the navbar scroling  problem

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
         $("nav ul").slideToggle(300);
     })
 })
+
+$("#navbar a").click(function(e) {
+    e.preventDefault();             //disable the "jumping"
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;          //.offset() is a jQuery function
+    $("html, body").animate({ scrollTop: targetPosition - 68 }, "slow");
+});
